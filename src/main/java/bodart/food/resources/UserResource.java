969e5d5d3430f603ee854aa94c9b6604cd7489d4
Bodart Gilles <38302@heb.be>
2015-03-16 20:07:35 +0100

    Iniutialisation serveur Food Angular + dropwizard

--- src/main/java/bodart/food/resources/UserResource.java
+++ src/main/java/bodart/food/resources/UserResource.java
@@ -1,14 +1,16 @@
 package bodart.food.resources;
 
 import bodart.food.db.controller.LoginCtrl;
-import com.sun.jersey.multipart.FormDataBodyPart;
+import bodart.food.db.exceptions.FoodMajorException;
 import com.sun.jersey.multipart.FormDataParam;
-import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,7 +18,6 @@ import javax.ws.rs.core.Response;
 @Path("/user")
 public class UserResource {
 
-    private final String SERVER_DOWNLOAD_LOCATION_FOLDER = "D:\\tmp\\";
     private final String sep = System.getProperty("file.separator");
     private final LoginCtrl logCtrl;
 
@@ -28,13 +29,18 @@ public class UserResource {
     @Path("/login")
     @Produces(MediaType.APPLICATION_JSON)
     public Response login(
-            @FormDataParam("email") String uniqueID,@FormDataParam("email") String uniqueID) {
-        String token = "token";
-        String message = "Loged in";
-        return Response
-                .ok(message)
-                .header("Food-token", token)
-                .build();
+            @FormDataParam("email") String mail,@FormDataParam("password") String password) {
+        try {
+            String token = logCtrl.logIn(mail, password);
+            String message = "Loged in";
+            return Response
+                    .ok(message)
+                    .header("Food-token", token)
+                    .build();
+        } catch (FoodMajorException | UnsupportedEncodingException | NoSuchAlgorithmException ex) {
+            Logger.getLogger(UserResource.class.getName()).log(Level.SEVERE, null, ex);
+            return Response.serverError().build();
+        }
     }
 
     @POST
@@ -46,10 +52,17 @@ public class UserResource {
             @FormDataParam("tel") String tel,
             @FormDataParam("password") String pass,
             @FormDataParam("adresse") String address) {
-
-        return Response
-                .ok(message)
-                .header("Food-token", token)
-                .build();
+        try {
+            String message = "user ";
+            message += (logCtrl.createUser(fName, lName, email, tel, pass, address) ? " created" :" not created");
+            return Response
+                    .ok(message)
+                    .build();
+        } catch (FoodMajorException | NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            Logger.getLogger(UserResource.class.getName()).log(Level.SEVERE, null, ex);
+            return Response
+                    .serverError()
+                    .build();
+        }
     }
 }
